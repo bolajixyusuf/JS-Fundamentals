d732c8e6d60c9a97907e956a03e81e9c2f129fab
bolajixyusuf <talkbuswithabdul@gmail.com>
2025-05-24 02:46:08 -0700

    Fix loop to handle negative and zero occurrences properly

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,14 +1,12 @@
 const arg = process.argv[2];
 const count = parseInt(arg);
 
-if (!count || count <= 0) {
+if (isNaN(count)) {
   console.log("Missing number of occurrences");
 } else {
-  let output = "";
   let i = 0;
   while (i < count) {
-    output += "C is fun\n";
+    console.log("C is fun");
     i++;
   }
-  console.log(output.trim());
 }
